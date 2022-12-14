b63464986078ef2ca9a1263a9212c765fce5b88d
Akbar Tri Nanda <akbartrinanda@gmail.com>
2022-12-14 11:31:10 +0700

    .

---
+++ dev/application-host/dist/app/app-config.ts
@@ -0,0 +1,16 @@
+export const CONFIG =
+{
+  basePath: "/dea",
+  api_key: 'ABCDEF123ERD456EABCDEF123ERD456E',
+  clientApp: "content-client",
+  clientPassword: "content-secret",
+
+  roleViewer: 1,
+  roleOperator: 2,
+  roleSupervisor: 3,
+  roleAdmin: 4,
+  roleSuperAdmin: 5,
+  roleViewerInternal: 6,
+  roleOperatorInternal: 7,
+  roleSupervisorInternal: 8
+};
