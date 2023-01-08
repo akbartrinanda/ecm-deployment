dd65f2951f29ec18e65d8c689591a6c23a451608
Akbar Tri Nanda <akbartrinanda@gmail.com>
2023-01-08 14:06:09 +0700

    .

--- dev/install.sh
+++ dev/install.sh
@@ -1,15 +1,14 @@
+sudo yum update -y
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
-sudo yum-config-manager  --add-repo https://download.docker.com/linux/centos/docker-ce.repo 
-sudo yum install docker-ce 
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo 
+sudo yum install -y docker-ce 
 sudo systemctl enable docker 
 sudo systemctl start docker 
 sudo systemctl status docker 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose 
 sudo chmod +x /usr/local/bin/docker-compose 
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose 
-sudo  docker-compose --version 
-sudo yum install git
-sudo git --version
+sudo docker-compose --version 
 git clone https://github.com/akbartrinanda/bpkb-deployment.git
 mkdir uploads-polda
 mkdir uploads-polri
