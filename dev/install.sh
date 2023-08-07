4a3bae33ae1338016539bd51c264ba439c2418db
Akbar Tri Nanda <akbartrinanda@gmail.com>
2023-08-07 10:55:16 +0700

    udpate

--- dev/install.sh
+++ dev/install.sh
@@ -1,29 +1,45 @@
-echo -e "\e[1;32msudo yum update -y\e[0m"
+echo -e "\e[1;32m sudo yum update -y \e[0m"
 sudo yum update -y
-echo -e "\e[1;32msudo yum install -y yum-utils device-mapper-persistent-data lvm2\e[0m"
+
+echo -e "\e[1;32m sudo yum install -y yum-utils device-mapper-persistent-data lvm2 \e[0m"
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
-echo -e "\e[1;32msudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo \e[0m"
+
+echo -e "\e[1;32m sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo  \e[0m"
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo 
-echo -e "\e[1;32msudo yum install -y docker-ce \e[0m"
+
+echo -e "\e[1;32m sudo yum install -y docker-ce \e[0m"
 sudo yum install -y docker-ce 
-echo -e "\e[1;32msudo systemctl enable docker \e[0m"
+
+echo -e "\e[1;32m sudo systemctl enable docker \e[0m"
 sudo systemctl enable docker 
-echo -e "\e[1;32msudo systemctl start docker \e[0m"
+
+echo -e "\e[1;32m sudo systemctl start docker \e[0m"
 sudo systemctl start docker 
-echo -e "\e[1;32msudo systemctl status docker \e[0m"
+
+echo -e "\e[1;32m sudo systemctl status docker \e[0m"
 sudo systemctl status docker 
-echo -e "\e[1;32msudo curl docker-compose \e[0m"
+
+echo -e "\e[1;32m sudo curl docker-compose \e[0m"
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose 
-echo -e "\e[1;32msudo chmod +x /usr/local/bin/docker-compose\e[0m" 
+
+echo -e "\e[1;32m sudo chmod +x /usr/local/bin/docker-compose \e[0m" 
 sudo chmod +x /usr/local/bin/docker-compose
-echo -e "\e[1;32msudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose \e[0m"
+
+echo -e "\e[1;32m sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose \e[0m"
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose 
-echo -e "\e[1;32msudo docker-compose --version \e[0m"
+
+echo -e "\e[1;32m sudo docker-compose --version \e[0m"
 sudo docker-compose --version 
-echo -e "\e[1;32mmkdir ~/uploads-bssn\e[0m"
-mkdir ~/uploads-bssn
-echo -e "\e[1;32mcd ~/bssn-deployment/dev/bssn-database-host && docker-compose down && docker-compose up -d --build\e[0m"
-cd ~/bssn-deployment/dev/bssn-database-host && sudo docker-compose down && sudo docker-compose up -d --build
-echo -e "\e[1;32mcd ~/bssn-deployment/dev/bssn-application-host && docker-compose down && docker-compose up -d --build\e[0m"
-cd ~/bssn-deployment/dev/bssn-application-host && sudo docker-compose down && sudo docker-compose up -d --build
+
+echo -e "\e[1;32m sudo docker network create ecm-dev-network \e[0m"
+sudo docker network create ecm-dev-network
+
+echo -e "\e[1;32m mkdir ~/file-uploads-ecm \e[0m"
+mkdir ~/file-uploads-ecm
+
+echo -e "\e[1;32m cd ~/ecm-deployment/dev/ecm-database-host && docker-compose down && docker-compose up -d --build \e[0m"
+cd ~/ecm-deployment/dev/ecm-database-host && sudo docker-compose down && sudo docker-compose up -d --build
+
+echo -e "\e[1;32m cd ~/ecm-deployment/dev/ecm-application-host && docker-compose down && docker-compose up -d --build \e[0m"
+cd ~/ecm-deployment/dev/ecm-application-host && sudo docker-compose down && sudo docker-compose up -d --build
 cd ~
